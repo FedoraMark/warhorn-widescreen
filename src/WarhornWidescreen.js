a8a908c4e8f3570fad4a2f6c1d9ef085f1a03c2e
FedoraMark <fedora.mark@icloud.com>
2024-09-29 14:23:54 -0400

    CSS

--- src/WarhornWidescreen.js
+++ src/WarhornWidescreen.js
@@ -1,12 +1,3 @@
-const $calendar_document = document.querySelector("#events_schedule_monthly_show .document");
-
-if ($calendar_document) {
+if (document.getElementById("events_schedule_monthly_show")) {
   console.log("Stretching the Warhorn calender");
-  $calendar_document.classList({
-    "max-width": "unset",
-    "margin": "0",
-    "padding-left": "2rem",
-    "padding-right": "2rem",
-    "box-sizing": "border-box"
-  });
 }
